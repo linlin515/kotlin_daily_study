97a7dc649922394c15334f514badc00f0f6d9bf5
SZ02204 <linruihang@xiongdi.cn>
2019-07-30 19:32:14 +0800

    day01 20190730

---
+++ app/src/main/java/class/Main.kt
@@ -0,0 +1,16 @@
+package `class`
+
+import `class`.day01.max
+
+/**
+ *
+ *
+ *@author LRH
+ *@email linruihang@xiongdi.cn
+ *@time 2019/7/30 19:14
+ */
+
+fun main(args: Array<String>) {
+    //比较两个数大小
+    println("the bigger is ${max(12, 333)}")
+}
---
+++ app/src/main/java/class/day01/day01.kt
@@ -0,0 +1,26 @@
+package `class`.day01
+
+/**
+ *
+ *
+ *@author LRH
+ *@email linruihang@xiongdi.cn
+ *@time 2019/7/30 19:15
+ */
+
+
+/**
+ * 方法定义
+ */
+fun max(a:Int,b:Int) :Int= if (a>b) a else b
+
+
+/**
+ * 变量定义
+ */
+
+//自动转换为Int类型
+val num = 12
+
+//指定特定类型
+val numSpecific:Int = 12
